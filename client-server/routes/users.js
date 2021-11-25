0f040406e1fe43a3c8cb9af6fe5f48c01b93a4d6
Isaac Caldas <marcos.felipe.clear@gmail.com>
2021-11-25 17:22:09 -0300

    Restify para acessar api rest - others servers

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var assert = require('assert');
+var restify = require('restify-clients');
 var router = express.Router();
+
+// creates a json client
+
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  client.get('/users', function (err, request, response, obj){
+    
+    assert.ifError(err);
+    res.end(JSON.stringify(obj, null, 2));
+  });
 });
 
 module.exports = router;
