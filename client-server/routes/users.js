20019838bb778bb34536d44700d756c132058092
Isaac Caldas <marcos.felipe.clear@gmail.com>
2021-11-26 13:43:21 -0300

    update client-server httprequest

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -13,9 +13,45 @@ router.get('/', function(req, res, next) {
     client.get('/users', function(err, request, response, obj) {
         assert.ifError(err);
 
-        res.end(JSON.stringify(obj, null, 2));
+        res.json(obj);
     });
+});
 
+router.get('/:id', function(req, res, next) {
+
+    client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+});
+
+router.put('/:id', function(req, res, next) {
+
+    client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+
+    client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+});
+
+
+router.post('/:id', function(req, res, next) {
+
+    client.post(`/users/`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
 });
 
 module.exports = router;
